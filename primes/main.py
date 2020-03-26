75de8e8a25ba494e5553c76744e8746c1248524e
svetelna <zu.miklankova@gmail.com>
2020-03-26 15:22:54 +0100

    adding function for discovering primes
    Signed-off-by: svetelna <zu.miklankova@gmail.com>

---
+++ primes/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from primes import primes_in_interval
+
+
+def main():
+    print(primes_in_interval(3, 9))
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ primes/primes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+def is_prime(n):
+    for i in range(2, n):
+        if (n % i == 0):
+            return False
+    return True
+
+
+def primes_in_interval(first, second):
+    primes = []
+    for i in range(first, second):
+        if is_prime(i):
+            primes.append(i)
+    return str(primes)
